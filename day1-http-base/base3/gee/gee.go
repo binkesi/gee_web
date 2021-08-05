36d3ec74047b02e54d656ce4cc51e16f7da964b1
sungn2 <sungn2@lenovo.com>
2021-08-05 23:10:45 +0800

    day1 engine

--- day1-http-base/base3/gee/gee.go
+++ day1-http-base/base3/gee/gee.go
@@ -1,5 +1,16 @@
 package gee
+
 import (
 	"fmt"
 	"net/http"
 )
+
+type HandlerFunc func(http.ResponseWriter, *http.Request)
+
+type Engine struct {
+	router map[string]HandlerFunc
+}
+
+func New() *Engine {
+	return &Engine{router: make(map[string]HandlerFunc)}
+}
