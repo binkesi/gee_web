b67ac9f3849ca13fcf952d232232629b2a410551
sungn2 <sungn2@lenovo.com>
2021-08-18 23:41:40 +0800

    Add route group.

---
+++ day4-group/go.mod
@@ -0,0 +1,3 @@
+module gee_group
+
+go 1.16
---
+++ day4-group/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"gee_group/gee"
+	"net/http"
+)
+
+func main() {
+	r := gee.New()
+	r.GET("/index", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Page.</h>")
+	})
+
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *gee.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello Gee!</h1>")
+		})
+		v1.GET("/hello", func(c *gee.Context) {
+			c.String(http.StatusOK, "hello %s, you're at %s", c.Query("name"), c.Path)
+		})
+	}
+
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *gee.Context) {
+			c.String(http.StatusOK, "hello %s, you're at %s", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *gee.Context) {
+			c.JSON(http.StatusOK, gee.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+
+	}
+
+	r.Run(":9999")
+}
