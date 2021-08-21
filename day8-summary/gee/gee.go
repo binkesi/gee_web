91ea676432ccc089c35eb8ffd0cce52e6a5fb8d4
sungn2 <sungn2@lenovo.com>
2021-08-21 14:25:11 +0800

    Add summary.

---
+++ day8-summary/gee/gee.go
@@ -0,0 +1,43 @@
+package gee
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Engine struct {
+	router map[string]HandlerFunc
+}
+
+type HandlerFunc func(http.ResponseWriter, *http.Request)
+
+func New() *Engine {
+	engine := &Engine{router: make(map[string]HandlerFunc)}
+	return engine
+}
+
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	key := method + "-" + pattern
+	engine.router[key] = handler
+}
+
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
+}
+
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
+}
+
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	key := req.Method + "-" + req.URL.Path
+	if handler, ok := engine.router[key]; ok {
+		handler(w, req)
+	} else {
+		fmt.Fprintf(w, "404 NOT FOUND: %s", req.URL)
+	}
+}
--- day8-summary/main.go
+++ day8-summary/main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"summary/gee"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "URL path is %s\n", req.URL.Path)
+	r := gee.New()
+	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, "URL.Path = %s\n", req.URL.Path)
 	})
-	http.ListenAndServe(":9999", nil)
+	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
+		for k, v := range req.Header {
+			fmt.Fprintf(w, "Headers[%q] = %q\n", k, v)
+		}
+	})
+	r.Run(":9999")
 }
