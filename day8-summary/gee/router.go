24939557ce8c95b209ce831b5d45a9497c647275
sungn2 <sungn2@lenovo.com>
2021-08-24 16:06:11 +0800

    Add route test and main func.

--- day8-summary/gee/router.go
+++ day8-summary/gee/router.go
@@ -67,9 +67,11 @@ func (r *router) getRoute(method string, path string) (*node, map[string]string)
 }
 
 func (r *router) handle(c *Context) {
-	key := c.Method + "-" + c.Path
-	if handler, ok := r.handler[key]; ok {
-		handler(c)
+	n, params := r.getRoute(c.Method, c.Path)
+	if n != nil {
+		c.Params = params
+		key := c.Method + "-" + c.Path
+		r.handler[key](c)
 	} else {
 		c.String(http.StatusNotFound, "404 NOT FOUND: %s", c.Path)
 	}
